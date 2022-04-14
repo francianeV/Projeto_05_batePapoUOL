75056409c7e6d7a06ffdda5b3ceacc8cb0007443
Franciane <franciane.penina@gmail.com>
2022-04-14 15:36:12 -0300

    mantendo a conexão do usuário a cada 4s

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 let conversas = [];
+const pedeNome = prompt("Digite seu nome: ");
+const name = {name: pedeNome};
 
+
+
+entradaChat();
 pegarConversas();
 setInterval(pegarConversas, 3000);
-//scroll();
+setInterval(manterConexao,4000);
 
 function pegarConversas(){
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
@@ -28,7 +33,7 @@ function renderizarConversas(){
 
     for (let i = 0; i < conversas.length; i++) {
         ulMensagems.innerHTML += `
-            <li><span class="time">${conversas[i].time}&nbsp</span> <span class="from"> ${conversas[i].from}&nbsp</span> <span class="text"> ${conversas[i].text} </span></li>
+            <li><span class="time">${conversas[i].time}&nbsp&nbsp</span> <span class="from"> ${conversas[i].from}&nbsp&nbsp</span> <span class="text"> ${conversas[i].text} </span></li>
         `;
     }
 }
@@ -42,7 +47,27 @@ function comparaMensagens(){
 }
 */
 
+function entradaChat(){
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",name);
 
-//function scroll(){
+    promise.then(pegarConversas);
+   // promise.catch(tratarErro);
+    
+}
+/*
+function tratarErro(error){
 
-//}
+    if(error.response.status === 400){
+        pedeNome = prompt("digite outro nome: ");
+    }else{
+        return pegarConversas();
+    }
+
+}
+*/
+
+function manterConexao(){
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status",name);
+
+    promise.then(pegarConversas);
+}
